f28e0e2cf989b89971367953144571e5f29589a0
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2023-12-21 16:43:32 -0300

    Working with While

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.IO.Pipes;
+using System.Security.Cryptography;
+using System.Globalization;
+using System.Xml.Schema;
+
+Console.WriteLine("Digite um numero! ");
+
+int NumeroLidos = int.Parse(Console.ReadLine());
+int soma = 0;
+
+
+
+while (NumeroLidos != 0)
+{
+
+    Console.WriteLine("Digite outro numero: ");
+    soma = soma + NumeroLidos;
+}
+Console.WriteLine("O valor dos numeros inseridos é de: " + soma);
